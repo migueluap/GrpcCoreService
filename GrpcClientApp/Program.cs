c959ce26a98748c6233a3cc53edbc8f8fec944be
Miguel Callo Luque <migueluap@hotmail.com>
2023-05-03 15:34:41 -0300

    Add project files.

---
+++ GrpcClientApp/Program.cs
@@ -0,0 +1,38 @@
+﻿using Grpc.Net.Client;
+using GrpcClientApp;
+
+//var message = new HelloRequest
+//{
+//    Name = "Sandeep Kumar"
+//};
+//var channel = GrpcChannel.ForAddress("http://localhost:5181");
+//var client = new Greeter.GreeterClient(channel);
+//var serverResponse = await client.SayHelloAsync(message);
+
+//Console.WriteLine(serverResponse.Message);
+//Console.WriteLine("Press any key to exit...");
+//Console.ReadKey();
+
+var channel = GrpcChannel.ForAddress("http://localhost:5181");
+var client = new Employee.EmployeeClient(channel);
+
+Console.WriteLine("Enter Employee ID....");
+var inputStr = Console.ReadLine();
+
+int empId = 0;
+if (!int.TryParse(inputStr, out empId))
+{
+    Console.WriteLine("This is invalid Employee ID, please exit and try again....");
+}
+else
+{
+    var emp = new GetEmpDetail { EmpId = empId };
+
+    var serverResponse = await client.GetEmpInformationAsync(emp);
+    Console.WriteLine("Employee Name | Role | Email ID | Department");
+    Console.WriteLine($"{serverResponse.EmpName} | {serverResponse.EmpRole} | {serverResponse.EmpEmail} | {serverResponse.EmpDepartment}");
+}
+Console.WriteLine("Press any key to exit...");
+Console.ReadKey();
+
+
